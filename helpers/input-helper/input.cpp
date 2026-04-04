4bf63522dc2b8c4092ee91b1ae4a2c5d3d299388
Michael Shirley <80654349+michaelshirley1@users.noreply.github.com>
2026-04-04 21:33:57 +1300

    Update input.cpp

---
+++ helpers/input-helper/input.cpp
@@ -0,0 +1,9 @@
+#include "input-helper/input.h"
+
+std::vector<float> input::loadImage(const std::string& path) {
+
+}
+
+void loadDataset(const std::string& rootFolder, const std::vector<std::string>& labels, std::vector<std::vector<float>>& inputs, std::vector<std::vector<float>>& actuals) {
+
+}
